bbaaa28e2230768aee1e57331e24bca9d3f3bcfa
mmar58 <rahmanapu118@gmail.com>
2022-11-30 06:19:34 +0600

    added title and spacing between title and graph

--- application/views/home.php
+++ application/views/home.php
@@ -10,45 +10,54 @@
 
     <!-- main css -->
     <style>
+        h1{
+            text-align: center;
+        }
+        .mainDiv{
+            margin-top: 3%;
+        }
         .selectTime{
             position: relative;
             left: 45%;
+            cursor: default;
         }
         .selectTime span{
             border: 1px black dotted;
             padding: 5px;
+            cursor: pointer;
         }
     </style>
 <script src="<?php echo base_url() ?>assets/js/Chart.js"></script>
 </head>
 
 <body>
-<div>
-    <div class="selectTime">Select Time <span>Week</span></div>
-    <div>
-        <canvas id="ReportChart" style="width:100%;height: 50%"></canvas>
+    <h1>Freelancer/Remote Job Report</h1>
+    <div class="mainDiv">
+        <div class="selectTime">Select Time <span>Week</span></div>
+        <div>
+            <canvas id="ReportChart" style="width:100%;height: 50%"></canvas>
+        </div>
+
     </div>
+    <script>
+        var xValues = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
+        var yValues = [7,8,8,9,9,9,10];
 
-</div>
-<script>
-    var xValues = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
-    var yValues = [7,8,8,9,9,9,10];
-
-    var reportChart=new Chart("ReportChart", {
-        type: "line",
-        data: {
-            labels: xValues,
-            datasets: [{
-                backgroundColor: "rgba(110,202,141,1)",
-                borderColor: "rgba(0,0,0,0.1)",
-                data: yValues
-            }]
-        },
-        options:{
-            legend: {display: false,}
-        }
-    });
-    console.log(reportChart)
-</script>
+        var reportChart=new Chart("ReportChart", {
+            type: "line",
+            data: {
+                labels: xValues,
+                datasets: [{
+                    backgroundColor: "rgba(110,202,141,1)",
+                    borderColor: "rgba(0,0,0,0.1)",
+                    data: yValues
+                }]
+            },
+            options:{
+                legend: {display: false,}
+            }
+        });
+        console.log(reportChart)
+    </script>
 </body>
 </html>
